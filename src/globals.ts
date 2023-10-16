855a872edf9940fd3bfebe200c33a3b3d5d66ea0
Ana <anasof@bu.edu>
2023-10-16 19:39:24 -0400

    "bu id added, figuring out what a x-f-key is tho"

--- src/globals.ts
+++ src/globals.ts
@@ -3,13 +3,18 @@
  * We've put a few here that we know you will need.
  * Fill in the blank for each one
  */
-export const MY_BU_ID = "";
+export const MY_BU_ID = "U84054577";
 export const BASE_API_URL = "https://spark-se-assessment-api.azurewebsites.net/api";
+
 // You can get this from Gradescope aka x-functions-key
 export const TOKEN = "";
+
 // This is a helper function to generate the headers with the x-functions-key attached
 export const GET_DEFAULT_HEADERS = () => {
   var headers = new Headers();
+  headers.append("x-functions-key", TOKEN); //attaching x-functions-key
+  headers.append("MY_BU_ID",MY_BU_ID); //attaching bu_id
+
   // You will need to add another header here
   // If you do not, the API will reject your request (:
   return headers;
