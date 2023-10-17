0f03be0d11764ca99c1c1c4b8a9408e1d7de6d28
Ana <anasof@bu.edu>
2023-10-16 20:54:55 -0400

    "added key from docs"

--- src/globals.ts
+++ src/globals.ts
@@ -7,7 +7,7 @@ export const MY_BU_ID = "U84054577";
 export const BASE_API_URL = "https://spark-se-assessment-api.azurewebsites.net/api";
 
 // You can get this from Gradescope aka x-functions-key
-export const TOKEN = "";
+export const TOKEN = "6se7z2q8WGtkxBlXp_YpU-oPq53Av-y_GSYiKyS_COn6AzFuTjj4BQ==";
 
 // This is a helper function to generate the headers with the x-functions-key attached
 export const GET_DEFAULT_HEADERS = () => {
