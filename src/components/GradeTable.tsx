ebab7b9d2969867f85eae71ebdd9c498076948c0
Ana <anasof@bu.edu>
2023-10-19 15:47:23 -0400

    "making my grade table. Not showing in browser"

--- src/components/GradeTable.tsx
+++ src/components/GradeTable.tsx
@@ -1,3 +1,5 @@
+import { DataGrid,GridColDef } from '@mui/x-data-grid';
+
 /**
  * You might find it useful to have some dummy data for your own testing.
  * Feel free to write this function if you find that feature desirable.
@@ -9,6 +11,17 @@ export function dummyData() {
   return [];
 }
 
+
+/** Making a table */
+const columns: GridColDef[]=[
+  {field:'ID',headerName:'Student ID',width:100},
+  {field:'StudentName',headerName:'Student Name',width:100},
+  {field:'ClassID',headerName:'Class ID',width:100},
+  {field:'ClassName',headerName:'Class Name',width:100},
+  {field:'Semester',headerName:'Semester',width:100},
+  {field:'FinalGrade',headerName:'Final Grade',width:100},
+
+];
 /**
  * This is the component where you should write the code for displaying the
  * the table of grades.
@@ -17,5 +30,6 @@ export function dummyData() {
  *
  */
 export const GradeTable = () => {
-  return <></>;
+  return <>
+  </>;
 };
